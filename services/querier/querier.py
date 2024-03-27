d045aa9ab87b10dccb37ee96fc17b6c8d6bbd188
huub <huubvandevoort@home.nl>
2024-03-27 08:24:50 +0100

    added traing on question-sql-pairs method to class

--- services/querier/querier.py
+++ services/querier/querier.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import sys
+import json
 
 import pandas as pd
 from vanna.remote import VannaDefault
@@ -11,9 +12,8 @@ from services.config import constants as c
 class DbQuerier:
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)  # sys.stdout = logging messages to console
     # level = general info messages.
-    training_data_path = '/training-data'
 
-    def __init__(self, sample_db_loc):
+    def __init__(self, sample_db_loc: str):
         """
         This function is used to initialize the class and give it its basic variables
         :param sample_db_loc: the location of where your database is stored. The database can be created if you run
@@ -21,6 +21,7 @@ class DbQuerier:
         """
         self.vanna = VannaDefault(api_key=c.VANNA_API_KEY, model=c.VANNA_MODEL_NAME)  # using the existing Vanna class
         self.vanna.connect_to_sqlite(sample_db_loc)  # connecting to the db
+        self.training_data_path = 'training-data'
         self.training_data = self.vanna.get_training_data()  # method from the original vanna class, returns Pandas df
         # this variable will show a pandas dataframe containing all the contextual training data
 
@@ -55,7 +56,7 @@ class DbQuerier:
         Trains the vanna remote context model on the query files listed in training-data/train_on_sql-queries
         :return: None
         """
-        sql_query_file_path = './services/querier/training-data/sql-queries'
+        sql_query_file_path = os.path.join('.', 'services', 'querier', 'training-data', 'sql-queries')
         query_files = [f for f in os.listdir(sql_query_file_path) if f.endswith('.train_on_sql')]
         for query in query_files:
             with open(os.path.join(sql_query_file_path, query), 'r') as q:
@@ -68,8 +69,7 @@ class DbQuerier:
         Trains the vanna remote context model on the documentation files listed in training-data/documentation
         :return: None
         """
-        logging.debug(os.getcwd())
-        documentation_file_path = "./services/querier/training-data/documentation"
+        documentation_file_path = os.path.join('.', 'services', 'querier', 'training-data', 'documentation')
         documentation_files = [f for f in os.listdir(documentation_file_path) if f.endswith('.txt')]
         for doc_file in documentation_files:
             with open(os.path.join(documentation_file_path, doc_file), 'r') as d:
@@ -79,10 +79,18 @@ class DbQuerier:
 
     def train_model_on_question_sql_pairs(self):
         """
-        TODO - Function to train the model on questions and resulting sql pairs.
+        Function to train the model on questions and resulting sql pairs.
         :return:
         """
-        pass
+        question_sql_pairs_path = os.path.join('.', 'services', 'querier', 'training-data', 'question-sql-pairs',
+                                               'question-sql-pairs.json')
+        if os.path.exists(question_sql_pairs_path):
+            with open(question_sql_pairs_path, 'r') as f:
+                question_sql_pairs_file = json.load(f)
+
+                for question, sql in question_sql_pairs_file.items():
+                    self.vanna.train(question=question, sql=sql)
+                logging.info("Trained on question-sql pairs.")
 
     def train_model(self, train_on_documentation: bool = True,
                     train_on_sql: bool = True,
--- services/querier/training-data/sql-queries/packages_received_oktnov.sql
+++ services/querier/training-data/sql-queries/packages_received_oktnov.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(DISTINCT cp.id) AS total_packages_received
 FROM collo_packages cp
 JOIN delivery_preference dp ON cp.account_id_hashed = dp.account_id_hashed
-WHERE cp.da_datum_acceptatie BETWEEN '2023-10-01' AND '2023-11-30'
+WHERE cp.da_datum_acceptatie BETWEEN '2023-10-01' AND '2023-11-30';
