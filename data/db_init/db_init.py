f16bf99243e72c339d243aa97fe109dbd098a9bf
patrykslomka <p.slomka@tilburguniversity.edu>
2024-03-28 11:32:48 +0100

    changed the db file to create a sample database

--- data/db_init/db_init.py
+++ data/db_init/db_init.py
@@ -51,86 +51,128 @@ class ColloPackages(Base):
     da_type_adres_gea = Column(String)          
     da_waarnemingsequence = Column(String)
 
-engine = create_engine('sqlite:///../PostNL_SQLite.sqlite')
-Base.metadata.create_all(engine)
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+def create_full_database():
+    engine = create_engine('sqlite:///../PostNL_Full_SQLite.sqlite')
+    Base.metadata.create_all(engine)
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
 
-# Read the CSV file
-df_delivery_preference = pd.read_csv(os.path.join('..', 'PostNL_account_delivery_preference_anonymized.csv'))
-date_format = "%Y-%m-%d %H:%M:%S.%f"
+    # Read the CSV files
+    df_delivery_preference = pd.read_csv(os.path.join('..', 'PostNL_account_delivery_preference_anonymized.csv'))
+    df_delivery_facts = pd.read_csv(os.path.join('..', 'PostNL_account_delivery_facts_anonymized.csv'))
+    df_collo_packages = pd.read_csv(os.path.join('..', 'PostNL_collo_packages_anonymized.csv'))
 
-# Insert delivery preference data into the database
-for index, row in df_delivery_preference.iterrows():
-    # Check if 'datelastupdated' is a string and convert it, otherwise use None or a default value
-    if isinstance(row['datelastupdated'], str):
-        datelastupdated = datetime.strptime(row['datelastupdated'], date_format)
-    else:
-        datelastupdated = None 
+    # Insert data into the database
+    for index, row in df_delivery_preference.iterrows():
+        datelastupdated = datetime.strptime(row['datelastupdated'], "%Y-%m-%d %H:%M:%S.%f") if isinstance(row['datelastupdated'], str) else None
+        datecreated = datetime.strptime(row['datecreated'], "%Y-%m-%d %H:%M:%S.%f") if isinstance(row['datecreated'], str) else None
+        new_delivery_preference = DeliveryPreference(
+                                    account_id_hashed=row['account_id_hashed'],
+                                    deliverypreference=row['deliverypreference'],
+                                    datelastupdated=datelastupdated,
+                                    datecreated=datecreated)
+        session.add(new_delivery_preference)
 
-    # Do the same for 'datecreated'
-    if isinstance(row['datecreated'], str):
-        datecreated = datetime.strptime(row['datecreated'], date_format)
-    else:
-        datecreated = None  # Or a default value
-
-    # Create a new DeliveryPreference object with datetime objects
-    new_delivery_preference = DeliveryPreference(
+    for index, row in df_delivery_facts.iterrows():
+        new_delivery_facts = DeliveryFacts(
                                 account_id_hashed=row['account_id_hashed'],
-                                deliverypreference=row['deliverypreference'],
-                                datelastupdated=datelastupdated,
-                                datecreated=datecreated)
-    session.add(new_delivery_preference)
+                                month_id=row['month_id'],
+                                number_of_parcels=row['number_of_parcels'],
+                                parcels_home_1st=row['parcels_home_1st'])
+        session.add(new_delivery_facts)
 
-df_delivery_facts = pd.read_csv(os.path.join('..', 'PostNL_account_delivery_facts_anonymized.csv'))
+    for index, row in df_collo_packages.iterrows():
+        new_collo_packages = ColloPackages(
+                                account_id_hashed=row['account_id_hashed'],
+                                dn_barcode=row['dn_barcode'],
+                                da_datum_voormelding=row['da_datum_voormelding'],
+                                da_datum_acceptatie=row['da_datum_acceptatie'],
+                                da_tijd_acceptatie=row['da_tijd_acceptatie'],
+                                sa_dag_sortering1=row['sa_dag_sortering1'],
+                                sa_datum_sortering1=row['sa_datum_sortering1'],
+                                sa_tijd_sortering1=row['sa_tijd_sortering1'],
+                                sa_datum_distributiecollectie=row['sa_datum_distributiecollectie'],
+                                sa_tijd_distributiecollectie=row['sa_tijd_distributiecollectie'],
+                                da_datum_herroutering_voor_up1=row['da_datum_herroutering_voor_up1'],
+                                da_tijd_herroutering_voor_up1=row['da_tijd_herroutering_voor_up1'],
+                                da_datum_eindstatus=row['da_datum_eindstatus'],
+                                da_tijd_eindstatus=row['da_tijd_eindstatus'],
+                                ma_gewicht=row['ma_gewicht'],
+                                ma_breedte=row['ma_breedte'],
+                                ma_lengte=row['ma_lengte'],
+                                ma_hoogte=row['ma_hoogte'],
+                                ma_volume=row['ma_volume'],
+                                PC4_gea=row['PC4_gea'],
+                                da_landcode_gea=row['da_landcode_gea'],
+                                da_resultaatgroepcode=row['da_resultaatgroepcode'],
+                                da_resultaatcode=row['da_resultaatcode'],
+                                da_type_adres_gea=row['da_type_adres_gea'],
+                                da_waarnemingsequence=row['da_waarnemingsequence'])
+        session.add(new_collo_packages)
 
-for index, row in df_delivery_facts.iterrows():
-    new_delivery_facts = DeliveryFacts(
-                            account_id_hashed=row['account_id_hashed'],
-                            month_id=row['month_id'],
-                            number_of_parcels=row['number_of_parcels'],
-                            parcels_home_1st=row['parcels_home_1st'])
-    session.add(new_delivery_facts)
+    session.commit()
+    print("Data has been inserted into the PostNL Full database")
 
-df_collo_packages = pd.read_csv(os.path.join('..', 'PostNL_collo_packages_anonymized.csv'))
+def create_sample_database():
+    engine = create_engine('sqlite:///../PostNL_Sample_SQLite.sqlite')
+    Base.metadata.create_all(engine)
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
 
-def convert_to_date(date_string):
-    return datetime.strptime(date_string, "%Y-%m-%d").date() if date_string else None
-def convert_to_time(time_string):
-    if time_string != 'Nvt':
-        return datetime.strptime(time_string, "%H:%M:%S").time() if time_string else None
-    else:
-        return None
+    # Read the CSV files
+    df_delivery_preference = pd.read_csv(os.path.join('..', 'PostNL_account_delivery_preference_anonymized.csv'))
+    df_delivery_facts = pd.read_csv(os.path.join('..', 'PostNL_account_delivery_facts_anonymized.csv'))
+    df_collo_packages = pd.read_csv(os.path.join('..', 'PostNL_collo_packages_anonymized.csv'))
 
-for index, row in df_collo_packages.iterrows():
-    new_collo_packages = ColloPackages(
-                            account_id_hashed=row['account_id_hashed'],
-                            dn_barcode=row['dn_barcode'],
-                            da_datum_voormelding=convert_to_date(row['da_datum_voormelding']),
-                            da_datum_acceptatie=convert_to_date(row['da_datum_acceptatie']),
-                            da_tijd_acceptatie=convert_to_time(row['da_tijd_acceptatie']),
-                            sa_dag_sortering1=convert_to_date(row['sa_dag_sortering1']),
-                            sa_datum_sortering1=convert_to_date(row['sa_datum_sortering1']),
-                            sa_tijd_sortering1=convert_to_time(row['sa_tijd_sortering1']),
-                            sa_datum_distributiecollectie=convert_to_date(row['sa_datum_distributiecollectie']),
-                            sa_tijd_distributiecollectie=convert_to_time(row['sa_tijd_distributiecollectie']),
-                            da_datum_herroutering_voor_up1=convert_to_date(row['da_datum_herroutering_voor_up1']),
-                            da_tijd_herroutering_voor_up1=convert_to_time(row['da_tijd_herroutering_voor_up1']),
-                            da_datum_eindstatus=convert_to_date(row['da_datum_eindstatus']),
-                            da_tijd_eindstatus=convert_to_time(row['da_tijd_eindstatus']),
-                            ma_gewicht=row['ma_gewicht'],
-                            ma_breedte=row['ma_breedte'],
-                            ma_lengte=row['ma_lengte'],
-                            ma_hoogte=row['ma_hoogte'],
-                            ma_volume=row['ma_volume'],
-                            PC4_gea=row['PC4_gea'],
-                            da_landcode_gea=row['da_landcode_gea'],
-                            da_resultaatgroepcode=row['da_resultaatgroepcode'],
-                            da_resultaatcode=row['da_resultaatcode'],
-                            da_type_adres_gea=row['da_type_adres_gea'],
-                            da_waarnemingsequence=row['da_waarnemingsequence'])
-    session.add(new_collo_packages)
+    # Insert a subset of data into the sample database - it's half now, but that can be changed if we want
+    num_records_to_insert = min(len(df_delivery_preference), len(df_delivery_facts), len(df_collo_packages)) // 2
 
-session.commit()
+    for index, row in df_delivery_preference.iloc[:num_records_to_insert].iterrows():
+        datelastupdated = datetime.strptime(row['datelastupdated'], "%Y-%m-%d %H:%M:%S.%f") if isinstance(row['datelastupdated'], str) else None
+        datecreated = datetime.strptime(row['datecreated'], "%Y-%m-%d %H:%M:%S.%f") if isinstance(row['datecreated'], str) else None
+        new_delivery_preference = DeliveryPreference(
+                                    account_id_hashed=row['account_id_hashed'],
+                                    deliverypreference=row['deliverypreference'],
+                                    datelastupdated=datelastupdated,
+                                    datecreated=datecreated)
+        session.add(new_delivery_preference)
 
-print("Data has been inserted into the PostNL database")
+    for index, row in df_delivery_facts.iloc[:num_records_to_insert].iterrows():
+        new_delivery_facts = DeliveryFacts(
+                                account_id_hashed=row['account_id_hashed'],
+                                month_id=row['month_id'],
+                                number_of_parcels=row['number_of_parcels'],
+                                parcels_home_1st=row['parcels_home_1st'])
+        session.add(new_delivery_facts)
+
+    for index, row in df_collo_packages.iloc[:num_records_to_insert].iterrows():
+        new_collo_packages = ColloPackages(
+                                account_id_hashed=row['account_id_hashed'],
+                                dn_barcode=row['dn_barcode'],
+                                da_datum_voormelding=row['da_datum_voormelding'],
+                                da_datum_acceptatie=row['da_datum_acceptatie'],
+                                da_tijd_acceptatie=row['da_tijd_acceptatie'],
+                                sa_dag_sortering1=row['sa_dag_sortering1'],
+                                sa_datum_sortering1=row['sa_datum_sortering1'],
+                                sa_tijd_sortering1=row['sa_tijd_sortering1'],
+                                sa_datum_distributiecollectie=row['sa_datum_distributiecollectie'],
+                                sa_tijd_distributiecollectie=row['sa_tijd_distributiecollectie'],
+                                da_datum_herroutering_voor_up1=row['da_datum_herroutering_voor_up1'],
+                                da_tijd_herroutering_voor_up1=row['da_tijd_herroutering_voor_up1'],
+                                da_datum_eindstatus=row['da_datum_eindstatus'],
+                                da_tijd_eindstatus=row['da_tijd_eindstatus'],
+                                ma_gewicht=row['ma_gewicht'],
+                                ma_breedte=row['ma_breedte'],
+                                ma_lengte=row['ma_lengte'],
+                                ma_hoogte=row['ma_hoogte'],
+                                ma_volume=row['ma_volume'],
+                                PC4_gea=row['PC4_gea'],
+                                da_landcode_gea=row['da_landcode_gea'],
+                                da_resultaatgroepcode=row['da_resultaatgroepcode'],
+                                da_resultaatcode=row['da_resultaatcode'],
+                                da_type_adres_gea=row['da_type_adres_gea'],
+                                da_waarnemingsequence=row['da_waarnemingsequence'])
+        session.add(new_collo_packages)
+
+    session.commit()
+    print("Data has been inserted into the PostNL Sample database")
