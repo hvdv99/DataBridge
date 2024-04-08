d1cb2987876ec09882705d852946406d2bcff5e4
mehdigreefhorst <mehdigreefhorst@gmail.com>
2024-04-08 12:56:43 +0200

    Took Huub's review comments and resolved them

--- services/ui/app.py
+++ services/ui/app.py
@@ -45,49 +45,67 @@ def home():
     - Jinja HTML templates: https://jinja.palletsprojects.com/en/3.1.x/templates/#template-inheritance
     """
     if request.method == 'GET':
-        return render_template('start_session.html')
+        return render_template('start-session.html')
     elif request.method == 'POST':
         pass
 
 
-@app.route('/vanna_table_view', methods=['GET', 'POST'])
+@app.route('/vanna-table-view', methods=['GET', 'POST'])
 def vanna_table_view():
     """
-    This route is what is invoked when the user clicks the submit button on the start_session.html page. Which asks the database
-    And then the page will display the first 10 rows of the data that is returned from the database but this happens in a different html file
+    This route is what is invoked when the user clicks the submit button on the start-session.html page.
+    Which asks the database
+    And then the page will display the first 10 rows of the data that is returned from the database
+     but this happens in a different html file
 
-    The else statement is for testing purposes, this loads an old html file with buttons for viewing the column descriptions
+    The else statement is for testing purposes,
+    this loads an old html file with buttons for viewing the column descriptions
     This also uses a premade SQL query
-    It is important to note that you need to give the parameter of the column description dict and the column description json. This is due to parsing not working properly. Else we will get issues with saving it later. So we use one to render in the uI and the json to send to the database to save.
+    It is important to note that you need to give the parameter of the column description dict
+     and the column description json. This is due to parsing not working properly.
+     Else we will get issues with saving it later.
+     So we use one to render in the uI and the json to send to the database to save.
     """
+
     if request.method == 'POST':
         question = request.form['session_question']
         email = request.form['session_email']
         subject = request.form['session_subject']
         sql_code = dbquery.generate_sql(question)
-        #If something goes wrong with generating the SQL if the question does not lead a valid SQL query, then we will return an error message
+        #If something goes wrong with generating the SQL if the question does not lead a valid SQL query,
+        # then we will return an error message
         try:
             df = dbquery.generate_sample_data(sql_query=sql_code).head(10)
 
         except Exception as e:
-            return render_template('start_session.html', error_message = f"I had an error generating the SQL, please rewrite question \n{str(e)}")
+            return render_template('start-session.html',
+                                    error_message = f"I had an error generating the SQL, please rewrite question \n{str(e)}")
         column_description_dict = dbquery.get_descriptions_for_given_columns(columns=df.columns.values)
 
+        return render_template('vanna-column-description.html',
+                               tables=[df.to_html(classes='data')],
+                               columns=df.columns.values,
+                               sql_code = sql_code,
+                               question = question,
+                               email = email,
+                               subject = subject,
+                               column_description_dict = column_description_dict)
 
-        return render_template('vanna_column_description.html', tables=[df.to_html(classes='data')], columns=df.columns.values, sql_code = sql_code, question = question, email = email, subject = subject, column_description_dict = column_description_dict)
     else:
-        sql_code = "SELECT month_id, SUM(parcels_home_1st) AS total_first_attempts FROM delivery_facts WHERE month_id LIKE '2023%' GROUP BY month_id ORDER BY total_first_attempts DESC LIMIT 10"
+        sql_code = """SELECT month_id, SUM(parcels_home_1st) AS total_first_attempts 
+                      FROM delivery_facts WHERE month_id LIKE '2023%' 
+                      GROUP BY month_id 
+                      ORDER BY total_first_attempts DESC LIMIT 10"""
         print("sql_code: ", sql_code)
 
         df = dbquery.generate_sample_data(sql_query=sql_code)
 
-        return render_template('vanna_table_view.html', tables=[df.to_html(classes='data')], titles=df.columns.values,
+        return render_template('vanna-table-view.html', tables=[df.to_html(classes='data')], titles=df.columns.values,
                                sql_code=sql_code)
 
-        return render_template('start_session.html')
 
 
-@app.route('/vanna_table_view/request_data', methods=['POST'])
+@app.route('/vanna-table-view/request-data', methods=['POST'])
 def request_data():
     """
     Takes the data from the request object and adds it to the database. Then it returns the same page with a success message
@@ -117,17 +135,18 @@ def request_data():
     return jsonify({"success": True, "message": "Data request received"})
 
 
-@app.route('/data_analyst_view', methods=['GET'])
+@app.route('/data-analyst-view', methods=['GET'])
 def data_analyst_view():
     """
-    This route is for the data analyst to view the requested data. It will show all the data that is requested from the database
+    This route is for the data analyst to view the requested data.
+    It will show all the data that is requested from the database
     """
     result = RequestedDataInit.query.all()
 
-    return render_template('data_analyst_view.html', requested_data=result)
+    return render_template('data-analyst-view.html', requested_data=result)
 
 
-@app.route('/view_request/<request_id>', methods=['GET'])
+@app.route('/view-request/<request_id>', methods=['GET'])
 def view_request(request_id):
     """
     This route is for the data analyst to view the details of a single row based on the ID of request.
@@ -140,7 +159,7 @@ def view_request(request_id):
 
     column_description_dict = dbquery.get_descriptions_for_given_columns(columns=column_list)
     example_table = dbquery.generate_sample_data(sql_query=result.sql_code).head(10)
-    return render_template('view_request.html', requested_data=result,
+    return render_template('view-request.html', requested_data=result,
                            tables = [example_table.to_html(classes='data')],
                            columns=example_table.columns.values,
                            column_description_dict = column_description_dict)
