09f1952da11e4ab1d440a2a76e25699ca3b5b10a
mehdigreefhorst <mehdigreefhorst@gmail.com>
2024-05-12 12:30:54 +0200

    Finished the whole update for the UI, it works now properly on full screen. The only error that is still there is with the column descriptions when a column name is different from the database (so when vanna makes up a new name or an aggregate name)

--- services/ui/app.py
+++ services/ui/app.py
@@ -90,7 +90,8 @@ def vanna_table_view():
             df = dbquery.generate_sample_data(sql_query=sql_code).head(10)
 
         except Exception as e:
-            error_text = f"I had an error generating the SQL, please rewrite question \n\n{str(e)}"
+            error_text = "I had a problem with your question, please try again."
+            print(f"I had an error generating the SQL, please rewrite question \n\n{str(e)}")
 
             return render_template('start-session.html', error_message=error_text)
 
